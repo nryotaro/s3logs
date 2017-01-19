fa77de58d663f1e1a1d20ba965cec811b24f6814
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-01-19 12:50:18 +0900

    save

--- s3logs.py
+++ s3logs.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import s3
+import tempfile as t
+import re
 
 bucket_name = 'foobar'
 
@@ -9,4 +11,9 @@ bucket = s3.Bucket(bucket_name)
 
 objs = bucket.objects.all() # all objects
 
+tempdir = t.mkdtemp() # dir
+# c = filter(lambda n: re.search('^logs/\d{4}', n), aa)
+#  aa = [c.key for c in a]
+
+# map(lambda e: re.search('^logs/(.+)',e).group(1), filter(lambda n: re.search('^logs/\d{4}', n), aa))
 
