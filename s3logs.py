b9186bcf983539b01f5ed267413cef8677b7854a
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-01-21 20:47:57 +0900

    parse a line

--- s3logs.py
+++ s3logs.py
@@ -4,6 +4,7 @@ import boto3
 import tempfile as t
 import re
 
+"""
 bucket_name = 'bucket_name'
 
 s3 = boto3.resource('s3')
@@ -23,3 +24,16 @@ for k in log_file_key_name_tuples:
     bucket.download_file(k['key'], tempdir + '/' + k['filename'])
 
 return [tempdir + '/' + k['filename'] for k in log_file_key_name_tuples]
+"""
+
+files = ["2017-01-14-15-17-49-A87702DEE4890EC6", "2017-01-15-00-26-01-D6E9D8DFD6946B9B"]
+
+def parse_log_line(line):
+    line_regex='^[^ ]+ [^ ]+ \[(.+)\] [^ ]+ [^ ]+ [^ ]+ ([^ ]+) ([^ ]+) \"([^\"]*)\" (\d{3}) [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ \"([^\"]*)\" \"([^\"]*)\" .+\r?\n$'
+    r = re.search(line_regex, line)
+    return {"time": r.group(1), "operation": r.group(2), "key": r.group(3), "request_url": r.group(4), "http_status": r.group(5), "referrer": r.group(6), "user_agent": r.group(7)}
+
+for a in files:
+    with open("logs/" + a, "r") as f:
+        for b in f.readlines():
+            print(parse_log_line(b))
