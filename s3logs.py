41eff675cc018938f043d53a554bcfb38b711949
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-01-21 22:01:07 +0900

    add list

--- s3logs.py
+++ s3logs.py
@@ -3,6 +3,7 @@
 import boto3
 import tempfile as t
 import re
+import os
 
 """
 bucket_name = 'bucket_name'
@@ -26,14 +27,35 @@ for k in log_file_key_name_tuples:
 return [tempdir + '/' + k['filename'] for k in log_file_key_name_tuples]
 """
 
-files = ["2017-01-14-15-17-49-A87702DEE4890EC6", "2017-01-15-00-26-01-D6E9D8DFD6946B9B"]
+
+
+files = os.listdir('logs') 
 
 def parse_log_line(line):
-    line_regex='^[^ ]+ [^ ]+ \[(.+)\] [^ ]+ [^ ]+ [^ ]+ ([^ ]+) ([^ ]+) \"([^\"]*)\" (\d{3}) [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ \"([^\"]*)\" \"([^\"]*)\" .+\r?\n$'
+    line_regex='^[^ ]+ [^ ]+ \[(.+)\] ([^ ]+) [^ ]+ [^ ]+ ([^ ]+) ([^ ]+) \"([^\"]*)\" (\d{3}) [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ \"([^\"]*)\" \"([^\"]*)\" .+\r?\n$'
     r = re.search(line_regex, line)
-    return {"time": r.group(1), "operation": r.group(2), "key": r.group(3), "request_url": r.group(4), "http_status": r.group(5), "referrer": r.group(6), "user_agent": r.group(7)}
+    return {"time": r.group(1), \
+            "remote_ip": r.group(2), \
+            "operation": r.group(3), \
+            "key": r.group(4), \
+            "request_url": r.group(5), \
+            "http_status": r.group(6), \
+            "referrer": r.group(7), 
+            "user_agent": r.group(8)}
 
-for a in files:
-    with open("logs/" + a, "r") as f:
-        for b in f.readlines():
-            print(parse_log_line(b))
+def access_logs(log_file_paths):
+    access=[]
+    for path in log_file_paths:
+        with open('logs/' + path, 'r') as f:
+            def filter_code(log_map): 
+                return log_map['key'].endswith('.html') and \
+                       log_map['operation'] == 'WEBSITE.GET.OBJECT' and \
+                       (not 'Googlebot' in log_map['user_agent'] and \
+                        not 'Baiduspider' in log_map['user_agent'] and \
+                        not 'bingbot' in log_map['user_agent']) and \
+                       log_map['http_status'] == "200"
+            access = access + filter(lambda e: filter_code(e), [parse_log_line(l) for l in f.readlines()])
+    return access
+
+a = access_logs(files)
+
